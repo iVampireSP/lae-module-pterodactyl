fd2822cec548ff84be056b2bbbc42b699c66cddc
iVampireSP.com <im@ivampiresp.com>
2022-09-03 17:29:00 +0800

    改进 地区页面

--- resources/views/locations/create.blade.php
+++ resources/views/locations/create.blade.php
@@ -5,11 +5,15 @@
     <form method="POST" action="{{ route('locations.store') }}">
         @csrf
         {{-- name --}}
-        <input type="text" name="name" placeholder="地区名称" />
+        显示名称：<input type="text" name="name" placeholder="地区名称" />
+        <br />
 
-        <input type="text" name="location_id" placeholder="翼龙面板里的地区 ID" />
+        翼龙面板里的地区 ID：<input type="text" name="location_id" placeholder="翼龙面板里的地区 ID" />
 
-        <input type="text" name="price" placeholder="基础价格(元)" />
+        <br />
+        基础价格：<input type="text" name="price" placeholder="基础价格(元)" />
+
+        <br />
 
         {{-- submit --}}
         <input type="submit" value="添加" />
--- resources/views/locations/edit.blade.php
+++ resources/views/locations/edit.blade.php
@@ -6,12 +6,12 @@
         @csrf
         @method('PUT')
          {{-- name --}}
-        <input type="text" name="name" placeholder="地区名称" value="{{ $location->name }}"/>
-
-        <input type="text" name="location_id" placeholder="翼龙面板里的地区 ID" value="{{ $location->location_id }}"/>
-
-        <input type="text" name="price" placeholder="基础价格(元)" value="{{ $location->price }}"/>
-
+        显示名称：<input type="text" name="name" placeholder="地区名称" value="{{ $location->name }}"/>
+        <br />
+        翼龙面板里的地区 ID:<input type="text" name="location_id" placeholder="翼龙面板里的地区 ID" value="{{ $location->location_id }}"/>
+        <br />
+        基础价格：<input type="text" name="price" placeholder="基础价格(元)" value="{{ $location->price }}"/>
+        <br />
         {{-- submit --}}
         <input type="submit" value="更新" />
 
