b79d0c8fc1ac4e6b4f278c3659c82a339fe2d54c
iVampireSP.com <im@ivampiresp.com>
2022-11-19 13:31:25 +0800

    改进 统计

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -16,28 +16,9 @@ class IndexController extends Controller
             return view('login');
         } else {
 
-            // $module =
-            $module = $this->http->get('modules')->json()['data'];
+            $years = $this->http->get('modules')->json()['data'];
 
-            $total = $module['transactions']['this_month']['balance'];
-
-            $drops = $module['transactions']['this_month']['drops'] / $module['rate'];
-
-            if ($drops < 0) {
-                $drops = 0;
-            }
-
-            $total += $drops;
-
-            $total = round($total, 2);
-
-            $module = [
-                'balance' => $module['transactions']['this_month']['balance'],
-                'drops' => $module['transactions']['this_month']['drops'],
-                'total' => $total,
-            ];
-
-            return view('index', ['module' => $module]);
+            return view('index', ['years' => $years]);
         }
     }
 
